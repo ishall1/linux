385ffe50f8037aeb1f56e33a7f9e67344742c67c
david <923320944@qq.com>
2018-01-06 01:53:47 +0800

    创建Xcode工程，用以查看代码

---
+++ LinuxSourceCode/LinuxSourceCode/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LinuxSourceCode
+//
+//  Created by david on 2018/1/6.
+//  Copyright © 2018年 david. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
